ca53b0587d17d52f35bdf7131f89ae8757421284
Brayan silva <84828739+brayan939827373@users.noreply.github.com>
2024-02-06 18:34:37 -0300

    Update Jogo dos numeros.py
    um jogo simples de acerto de numeros aleatorios, facíl e intuitivo 
    e com tratamento de erros

--- Jogo dos numeros.py
+++ Jogo dos numeros.py
@@ -1,55 +1,83 @@
+# Criador         : Brayan vieira 
+# função          : jogo de acerto de numero aleatorio 
+# versão          : 2.2
+# data da criação : 06/2/2024
+
 import random 
-import sys
+import platform
+import os
+#----------------------------------------------------------------------------------------------------------------
+#                                       Funções extras para deixar o codigo mais limpo
 ESPACO = 5 * "\n"
 BARRAS = 30 * "-"
+#----------------------------------------------------------------------------------------------------------------
+#                                            Erros do programa salvos
+ERRO12 = "você não deve inserir numeros menores que 0"
+ERRO10 = "você não deve inserir numeros maiores que 10"
+ERRO5 = " erro insira somente numeros "
+#----------------------------------------------------------------------------------------------------------------
+#                                       identificando o sistema operacional para utilizar o clear 
+SISTEMA = platform.system()
+if SISTEMA == "Windows":
+    limpar = "cls"
+elif SISTEMA == "Linux":
+    limpar = "clear"
+#----------------------------------------------------------------------------------------------------------------
+#                                       Cores do programa 
 class color :
     RED = "\033[91m"
     GREEN = "\033[92m"
     RESET = "\033[0m"
+
 while True:
 #-----------------------------------------------------------------------------------------------------------------
 #                                        Menu de inicio
-    print(ESPACO)
-    print("bem vindo ao jogo de acertos, irei gerar um numero de 1 a 10 e vc deve acertar qual e o numero ")
-    print(ESPACO)
-
+    os.system(limpar)
+    print("bem vindo ao jogo de acertos, irei gerar um numero de 1 a 10 e vc deve acertar qual e o numero  \n ")
 #-----------------------------------------------------------------------------------------------------------------
-#                                          tratamento de erros 
+#                                         Erro de caracter invalido 
     try:
         num1 = int(input(" Insira algum numero para tentar adivinhar o meu :  "))
-    except:
         print(BARRAS)
-        print(" erro insira somente numeros ")
+    except ValueError:
+        print("")
         print(BARRAS)
-        sys.exit()
-
+        exit(ERRO5)
+#----------------------------------------------------------------------------------------------------------------
+#                                Erro de numero invalido (maior que 10)
 
     if num1 > 10:
         print(BARRAS)
-        print("você não deve inserir numeros maiores que 10 ")
+
+        print(ERRO10)
+
         print(BARRAS)
-        sys.exit()
+        exit(10)
+#----------------------------------------------------------------------------------------------------------------
+#                                       erro de numero invalido (menor que 0)
     if num1 < 0:
         print(BARRAS)
-        print("você não deve inserir numeros menores que 0 ")
+        print(ERRO12)
         print(BARRAS)
-        sys.exit()
+        exit(12)
 
 #-----------------------------------------------------------------------------------------------------------------
 #                                       gerando o numero 
     my_num = random.randint(1,10)
-
-
 #-----------------------------------------------------------------------------------------------------------------
-    #                                             verificador de acertos
+#                                   configurando o acerto 
     if num1 == my_num:
-        print(ESPACO)
+        os.system(limpar)
         print(color.GREEN + "você acertou meu numero parabens " + color.RESET)
         print(ESPACO)
+#----------------------------------------------------------------------------------------------------------------
+#                                   configurando o erro 
     else:
-        print(ESPACO)
+        os.system(limpar)
         print(color.RED + f" Você errou meu numero e : {my_num}" + color.RESET)
         print(ESPACO)
+#----------------------------------------------------------------------------------------------------------------
+#                                       quer reiniciar o looping? 
     dnv = input("quer jogar novamente ? [S] sim [N] não : ").lower().startswith("s")
     if dnv == True:
         continue
